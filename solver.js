516b275a01655f509d11d5362344e7d5dc8dd420
Shivam Garg <142667097+Shivamgargdtu@users.noreply.github.com>
2025-10-24 23:19:05 +0530

    Add files via upload

---
+++ solver.js
@@ -0,0 +1,97 @@
+const boardDiv = document.getElementById('board');
+
+for (let i = 0; i < 81; i++) {
+  const cell = document.createElement('input');
+  cell.maxLength = 1;
+  cell.type = 'text';
+  boardDiv.appendChild(cell);
+  cell.addEventListener('input', () => {
+    const val = cell.value;
+    if (!val.match(/^[1-9.]?$/)) {
+      cells.forEach(c => (c.value = ''));
+    }
+  });
+}
+
+const cells = document.querySelectorAll('#board input');
+
+function getBoard() {
+  let board = [];
+  for (let i = 0; i < 9; i++) {
+    let row = [];
+    for (let j = 0; j < 9; j++) {
+      let val = cells[i * 9 + j].value;
+      row.push(val === '' ? '.' : val);
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+function setBoard(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      cells[i * 9 + j].value = board[i][j] === '.' ? '' : board[i][j];
+    }
+  }
+}
+
+function check(board, i, j, val) {
+  const row = i - (i % 3);
+  const col = j - (j % 3);
+
+  for (let x = 0; x < 9; x++)
+    if (board[x][j] === val) return false;
+
+  for (let y = 0; y < 9; y++)
+    if (board[i][y] === val) return false;
+
+  for (let x = 0; x < 3; x++)
+    for (let y = 0; y < 3; y++)
+      if (board[row + x][col + y] === val) return false;
+
+  return true;
+}
+
+
+function solve(board, empty, index, n) {
+  if (index.value >= n) return true;
+
+  let i = empty[index.value].first;
+  let j = empty[index.value].second;
+
+  for (let val = 1; val <= 9; val++) {
+    let ch = val.toString();
+    if (check(board, i, j, ch)) {
+      board[i][j] = ch;
+      index.value++;
+      if (solve(board, empty, index, n)) return true;
+      index.value--;
+      board[i][j] = '.';
+    }
+  }
+  return false;
+}
+
+function solveSudoku(board) {
+  let empty = [];
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] === '.') empty.push({ first: i, second: j });
+    }
+  }
+
+  let n = empty.length;
+  let index = { value: 0 };
+  solve(board, empty, index, n);
+}
+
+document.getElementById('solveBtn').addEventListener('click', () => {
+  let board = getBoard();
+  solveSudoku(board);
+  setBoard(board);
+});
+
+document.getElementById('clearBtn').addEventListener('click', () => {
+  cells.forEach(c => (c.value = ''));
+});
